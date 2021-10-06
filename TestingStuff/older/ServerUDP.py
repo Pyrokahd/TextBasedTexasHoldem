da8e216c3113c70ea92396ad9bfb032d09aef731
unknown <christian@wherzog.de>
2021-10-06 09:50:07 +0200

    removed testing folders

--- TestingStuff/older/ServerUDP.py
+++
@@ -1,9 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # INET = IPv4, DGRAM = UDP
-try:
-    s.bind(("", 50000))  # keine Adresse = Alle auch localhost
-    while True:
-        daten, addr = s.recvfrom(1024)  # maximale Paketgröße 1024 it
-        print("[{}] {}".format(addr[0], daten.decode()))
-finally:
-    s.close()
